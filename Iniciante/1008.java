e9dc8b622749dee8d143fc7ab03ad638dfbe42b1
Wellington Ramos <wellingtonraam@gmail.com>
2019-02-09 11:49:20 -0200

    Exercicio 1010
    Resolução do exercicio 1010

--- Iniciante/1008.java
+++ Iniciante/1008.java
@@ -1,19 +1,21 @@
 /**
-Diferença
+Salário
 Adaptado por Neilor Tonin, URI  Brasil
 
 Timelimit: 1
-Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
 Entrada
-O arquivo de entrada contém 4 valores inteiros.
+O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
 
 Saída
-Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
 */
 
 
 import java.util.Scanner;
+
+import java.text.DecimalFormat;
 
 
 
@@ -23,21 +25,29 @@ public class Main {
 
     public static void main(String args[]) {
 
- Scanner in = new Scanner (System.in);
-        
-int dif,a,b,c,d;
-        
-a = in.nextInt();
-        
-b = in.nextInt();
-        
-c = in.nextInt();
-        
-d = in.nextInt();
-        
-dif = ((a*b) - (c*d));
-        
-System.out.println("DIFERENCA = " + dif);  
+
+DecimalFormat df = new DecimalFormat("0.00");
+
+Scanner in = new Scanner(System.in);
+
+
+int id, horas;
+
+double valorHora, salario;
+
+
+
+id = in .nextInt();
+
+horas = in .nextInt();
+
+valorHora = in .nextDouble();
+
+
+salario = (double) horas * valorHora;
+
+
+System.out.println("NUMBER = " + id + "\nSALARY = U$ " + df.format(salario));
   }
 
 }
