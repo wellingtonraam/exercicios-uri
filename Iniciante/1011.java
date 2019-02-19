6bc27b8dbab023915c3146ca10b5e3d2976687ac
Wellington Ramos <wellingtonraam@gmail.com>
2019-02-18 22:27:08 -0300

    Update 1011.java

--- Iniciante/1011.java
+++ Iniciante/1011.java
@@ -1,7 +1,7 @@
 /**
 Diferença
 Adaptado por Neilor Tonin, URI  Brasil
-
+21
 Timelimit: 1
 Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
 
